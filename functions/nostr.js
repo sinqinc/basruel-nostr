b6952acc4f33dedb7eddf6b805c0c05bc12f6909
SINQ <info@sinq.ca>
2022-12-22 09:57:57 -0500

    fixed typo

--- functions/nostr.js
+++ functions/nostr.js
@@ -7,7 +7,6 @@ let val = {
     "basruel": "08da8aabac2a2e031131d0ba626b18c9a8cb98d5d2ebbcbdc62efb48e9efbddd"
   }
 };
-
     const json = JSON.stringify(val);
     return new new Response(json, {
       headers: {
