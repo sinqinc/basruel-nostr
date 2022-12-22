27e78ca850aa33a7257aeb184c226dd793b8ad42
SINQ <info@sinq.ca>
2022-12-22 09:57:12 -0500

    added json

--- functions/nostr.js
+++ functions/nostr.js
@@ -1,12 +1,18 @@
 
 
 export function onRequest(context) {
-  $.getJSON("/nostr.json", function(val) {
+  //$.getJSON("/nostr.json", function(val) {
+let val = {
+  "names": {
+    "basruel": "08da8aabac2a2e031131d0ba626b18c9a8cb98d5d2ebbcbdc62efb48e9efbddd"
+  }
+};
+
     const json = JSON.stringify(val);
     return new new Response(json, {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
-      },
+      }
     });
   });
 
