c14e1fcbb166e482d9fd6a1cb4a817d128aa56d5
SINQ <info@sinq.ca>
2022-12-22 10:01:01 -0500

    test with external json

--- functions/nostr.js
+++ functions/nostr.js
@@ -1,16 +1,17 @@
 
 
 export function onRequest(context) {
-  //$.getJSON("/nostr.json", function(val) {
-let val = {
-  "names": {
-    "basruel": "08da8aabac2a2e031131d0ba626b18c9a8cb98d5d2ebbcbdc62efb48e9efbddd"
-  }
-};
+  $.getJSON("/nostr.json", function(val) {
+//let val = {
+//  "names": {
+//    "basruel": "08da8aabac2a2e031131d0ba626b18c9a8cb98d5d2ebbcbdc62efb48e9efbddd"
+//  }
+//};
     const json = JSON.stringify(val);
-    return new new Response(json, {
+    return new Response(json, {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
       }
     });
+  });
 }
